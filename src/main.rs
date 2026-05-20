b1de192fc86927919bed75341207abf6a798cb5d
immr108 <immr108@gmail.com>
2026-05-20 18:35:33 +0900

    削除前の確認を追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use aws_sdk_s3::Client;
 use dialoguer::Select;
+use std::io;
 
 #[tokio::main]
 async fn main() {
@@ -14,26 +15,33 @@ async fn main() {
             println!("error: {}", e);
             return;
         }
-
     };
 
     let buckets = response.buckets();
-    let bucket_names: Vec<&str> = buckets
-    .iter()
-    .filter_map(|b| b.name())
-    .collect();
+    let bucket_names: Vec<&str> = buckets.iter().filter_map(|b| b.name()).collect();
 
-    if bucket_names.is_empty(){
+    if bucket_names.is_empty() {
         println!("バケットが存在しません");
         return;
     }
 
     let selection = Select::new()
-    .with_prompt("削除するバケットを選択してください")
-    .items(&bucket_names)
-    .interact()
-    .expect("選択エラー");
+        .with_prompt("削除するバケットを選択してください")
+        .items(&bucket_names)
+        .interact()
+        .expect("選択エラー");
 
     println!("選択されたバケット: {}", bucket_names[selection]);
+    println!("本当に削除しますか？削除する場合は[delete]と入力してください: ");
 
+    let mut confirm = String::new();
+    io::stdin().read_line(&mut confirm).expect("入力エラー");
+    let confirm = confirm.trim();
+
+    if confirm != "delete" {
+        println!("キャンセルしました");
+        return;
+    }
+
+    println!("削除処理を開始します。")
 }
